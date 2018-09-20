c189f498cbc63c62fdcfd5f8b6a2db0909abb887
Ricardo Torquato <torquatto@msn.com>
2018-09-20 14:18:40 -0300

    Implementation of date functions to pass on tests and fix some tests expectations

--- tests/test-date.js
+++ tests/test-date.js
@@ -6,7 +6,25 @@ const date = require( '../libs/date' )
     , chai = require('chai')
     , assert = chai.assert;
 
-describe('Testing functions for date validation', function () {
+describe('Testing functions for date validation format', function () {
+    it('should return true when the format is valid', function () {
+        assert.isTrue( date.isFormatValid('2018-09-01'));
+      });
+    
+    it('should return true when the format is valid even when the value is not', function () {
+        assert.isTrue( date.isFormatValid('2018-09-32'));
+      });
+    
+    it('should return false when the format is not valid', function () {
+        assert.isFalse( date.isFormatValid('01/09/2018'));
+      });
+
+    it('should return false when the format is not even a date', function () {
+        assert.isFalse( date.isFormatValid('Hello World'));
+      });
+});
+
+describe('Testing functions for date validation value', function () {
     it('should return true because the date is valid', function () {
         assert.isTrue( date.isValid('2018-09-19') );
       });
@@ -16,20 +34,20 @@ describe('Testing functions for date validation', function () {
       });
 
     it('should throw error because of invalid date format', function () {
-        assert.throw( date.isValid('19/09/2018') );
+        assert.throws( () => date.isValid('19/09/2018') );
       });
 });
 
 describe('Testing functions for date conversion', function () {
     it('should return the timestamp value of the date', function () {
-        assert.equal( date.isValid('2018-09-19'), 1568862000 );
+        assert.equal( date.toTimestamp('2019-09-19'), 1568851200 );
       });
     
     it('should throw error because of invalid date value', function () {
-        assert.throws( () => date.isValid('2018-09-32') );
+        assert.throws( () => date.toTimestamp('2018-09-32') );
       });
     
     it('should throw error because of invalid date format', function () {
-        assert.throws( () => date.isValid('19/09/2018') );
+        assert.throws( () => date.toTimestamp('19/09/2018') );
       });
 });
