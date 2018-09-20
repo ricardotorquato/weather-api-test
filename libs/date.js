5fc1eb268efe6f63cf87f4edf0eaeb7b64ed299b
Ricardo Torquato <ricardotorquato@MacBook-Pro-de-Ricardo.local>
2018-09-20 14:18:40 -0300

    Implementation of date functions to pass on tests and fix some tests expectations

--- libs/date.js
+++ libs/date.js
@@ -1,6 +1,26 @@
 const date = {};
 
-date.isValid = (myDate) => true;
-date.toTimestamp = (myDate) => 0;
+date.isFormatValid = (myDate) => /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/.test(myDate);
+
+date.isValid = (myDate) => {
+    
+    if ( !date.isFormatValid(myDate) ) {
+        throw new Error('Invalid date format. Use yyyy-mm-dd');
+    }
+
+    return !isNaN(Date.parse(myDate));
+};
+
+date.toTimestamp = (myDate) => {
+    if ( !date.isFormatValid(myDate) ) {
+        throw new Error('Invalid date format. Use yyyy-mm-dd format.');
+    }
+
+    if ( !date.isValid(myDate) ) {
+        throw new Error('Invalid date value. Verify and try again.');
+    }
+
+    return Date.parse(myDate) / 1000;
+};
 
 module.exports = date;
