3bcee956c9e44698dde3e07eff0ba28578642e48
Ricardo Torquato <ricardotorquato@MacBook-Pro-de-Ricardo.local>
2018-09-19 23:57:45 -0300

    Adding weather module and testing it

---
+++ tests/test-weather.js
@@ -0,0 +1,33 @@
+/*
+ * This test is for the cities module
+ */
+
+const weather = require( '../weather' )
+    , chai = require('chai')
+    , assert = chai.assert;
+
+describe('Testing retrieving data from weather module', function () {
+    it('should retrieve all weather information of a city', function () {
+        assert.equal( weather.findByCityId(3992619).length, 16 );
+      });
+
+    it('should not find information with this id', function () {
+        assert.notExists( weather.findByCityId(1) );
+      });
+    
+    it('should not find information with a string as id', function () {
+        assert.notExists( weather.findByCityId('3992619') );
+      });
+
+    it('should allow start and end date to filter the weather information', function () {
+        assert.equal( weather.findByCityId(3992619, '2017-03-12', '2017-03-21').length, 9 );
+      });
+    
+    it('should throws errors on invalid date format', function () {
+        assert.throws( () => weather.findByCityId(3992619, '12/03/2017', '21/03/2017') );
+      });
+    
+    it('should throws errors on invalid date value', function () {
+        assert.throws( () => weather.findByCityId(3992619, '2017-03-32', '2017-04-32') );
+      });
+});
---
+++ weather.js
@@ -0,0 +1,10 @@
+const data = require( './data/weather_list.json' )
+    , weather = {};
+
+weather.findByCityId = (cityId) => {
+    myData = data.filter( w => w.cityId === cityId )[0];
+    
+    return typeof(myData) === 'object' ? myData.data : null;
+}
+
+module.exports = weather;
