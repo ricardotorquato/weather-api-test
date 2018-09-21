7e916ef992ed9f87b127f7c87cb77cc9e869e73e
Ricardo Torquato <torquatto@msn.com>
2018-09-20 23:24:50 -0300

    Rethinking routes design

--- controllers/cities.js
+++ controllers/cities.js
@@ -2,7 +2,6 @@ const controller = {};
 
 controller.get = async (req, res, next) => next('get not implemented yet');
 controller.getById = async (req, res, next) => next('getById not implemented yet');
-controller.getWeather = async (req, res, next) => next('getWeather not implemented yet');
-controller.getWeatherById = async (req, res, next) => next('getWeatherById not implemented yet');
+controller.getByIdWithWeather = async (req, res, next) => next('getByIdWithWeather not implemented yet');
 
 module.exports = controller;
