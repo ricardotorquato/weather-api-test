1115eadccb21fb91e705e4a2c89dece4f25cee45
Ricardo Torquato <torquatto@msn.com>
2018-09-20 21:32:42 -0300

    Adding tests of relation between cities and weather information

--- libs/cities.js
+++ libs/cities.js
@@ -4,4 +4,13 @@ const data = require( '../data/city_list.json' )
 cities.findAll = () => data;
 cities.findById = (id) => data.filter( city => city.id === id )[0];
 
+cities.findWithWeather = (params = {}) => {
+    const find = {};
+
+    find.all = () => [];
+    find.byId = (id) => null;
+
+    return find;
+}
+
 module.exports = cities;
--- tests/libs/test-cities.js
+++ tests/libs/test-cities.js
@@ -23,3 +23,70 @@ describe('Testing retrieving data from cities module', function () {
         assert.notExists( cities.findById('3531732') );
       });
 });
+
+describe('Testing retrieving data from cities related to weather', function () {
+    it('should retrieve only the cities with weather informed and their weather information', function () {
+        const data = cities.findWithWeather().all();
+        
+        // We should have two cities with weather
+        assert.equal( data.length, 2 );
+        
+        // Both should have the "weather" node
+        assert.equal( data.filter( d => typeof(d.weather) === 'object').length, 2 );
+      });
+  
+    it('should retrieve only the cities with weather informed and their weather information filtered by range of date', function () {
+          const params = { 
+              startDate : '2017-03-12', 
+              endDate   : '2017-03-21'
+          };
+
+          const data = cities.findWithWeather(params).all();
+
+          // We should still have two cities with weather
+          assert.equal( data.length, 2 );
+        
+          // Both should have the "weather" node
+          assert.equal( data.filter( d => typeof(d.weather) === 'object').length, 2 );
+
+          // The weather node filtered should have less items
+          assert.equal( data.filter( d => d.weather.length === 9 ).length, 2 );
+      });
+    
+    it('should retrieve just one city with its weather information', function () {
+        const city = cities.findWithWeather().byId(3992619);
+
+        assert.exists( city.weather );
+      });
+    
+    it('should retrieve just one city with its weather information filtered by a range of dates', function () {
+        const params = { 
+            startDate : '2017-03-12', 
+            endDate   : '2017-03-21'
+        };
+
+        const city = cities.findWithWeather(params).byId(3992619);
+
+        assert.exists( city );
+        assert.exists( city.weather );
+        assert.equal( city.weather.length, 9 );
+      });
+    
+    it('should throws error because of invalid date format', function () {
+        const params = { 
+            startDate : '12/03/2017', 
+            endDate   : '21/03/2017'
+        };
+
+        assert.throws( () => cities.findWithWeather(params) );
+      });
+
+    it('should throws error because of invalid date value', function () {
+        const params = { 
+            startDate : '2017-03-32', 
+            endDate   : '2017-03-32'
+        };
+
+        assert.throws( () => cities.findWithWeather(params) );
+      });
+});
