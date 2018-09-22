bf724ac323f2b0044a85e963384aa8b5715eaaca
Ricardo Torquato <torquatto@msn.com>
2018-09-22 10:59:49 -0300

    Changing the api entry point to /api instead of / directly

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require( 'express' ).Router();
 
-router.use( '/v1', require('./v1') );
+router.use( '/api/v1', require('./v1') );
 router.use( '/doc', require('./doc') );
 
 module.exports = router;
